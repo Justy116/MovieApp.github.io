e56fbae6b749e4df251bd6c545e534212ca700ab
Justy116 <86831332+Justy116@users.noreply.github.com>
2022-03-24 22:26:04 +0100

    whatever

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie.js";
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e5a10c321bbd6c822e0ff030e4b57604";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=e5a10c321bbd6c822e0ff030e4b57604&query=";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then(data => {
+        setMovies(data.results);
+      })
+  }, []);
+
+  const handleOnSubmit = (e) =>{
+      e.preventDefault();
+
+      if(searchTerm){
+        fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then(data => {
+          setMovies(data.results);
+          console.log(data);
+        })
+      }
+  }
+
+  const handleOnChange = (e) =>{
+    setSearchTerm(e.target.value);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <form onSubmit={handleOnSubmit}>
+          <input className="search" type="search" placeholder="Search..." value={searchTerm} onChange={handleOnChange}/>
+        </form>
       </header>
+      <div className="movie-container">
+        {movies.map((movie) =>
+          <Movie key={movie.id} {...movie} />
+        )}
+      </div>
     </div>
+
   );
 }
 
